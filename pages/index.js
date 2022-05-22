ebab39355600ade259bb3ee8cfec0b112610ce8f
kherin.eth <kherincalderon@gmail.com>
2022-05-21 18:14:05 -0600

    [ init ] project

---
+++ pages/index.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+import Product from "../components/Product";
+import HeadComponent from '../components/Head';
+
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+
+// Constants
+const TWITTER_HANDLE = "kherincalderon";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+  // This will fetch the users' public key (wallet address) from any wallet we support
+  const { publicKey } = useWallet();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://media.giphy.com/media/3mJDZZDbz6nScuHvyA/giphy.gif" alt="ucl" />
+
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>    
+    </div>
+  );
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+
+  return (
+    <div className="App">
+      <HeadComponent/>
+      <div className="container">
+        <header className="header-container">
+          <p className="header">Welcome to Retr⚽️Football Club®</p>
+          <p className="sub-text">The only legendary t-shirts store that accepts Solana Pay</p>
+        </header>
+
+        <main>
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
+        </main>
+
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built by @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
